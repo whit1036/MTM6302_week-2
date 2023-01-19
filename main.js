e01e8cf9640b1c7b1e56bdf58960798d9c41796b
Brennan Whitworth <whit1036@algonquinlive.com>
2023-01-19 10:32:18 -0500

    test

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
-// Variables
-
-// avoid keywords
-// avoid variable names already assigned
-
-//var firstName = "Brennan";
-//firstName = "Jane";
-//console.log(firstName)
-
-//Let not going to change
-
-//let secondName = "Whitworth";
-{
-    //let secondName = "Brennan";
-}
-//console.log(secondName)
-
-//Const going to change
-
 const firstName = "Joe"
 
-const firstName = "Jane"
-
 console.log(firstName)
+
+const secondName = "Ram"
+console.log(firstName + " " + secondName + "Whitworth")
+
+const description = `My name is: ${firstName}
+My second name: ${secondName}. ${2+2}`
+
+console.log(description)
+
+//5. Number
+//represent floating-point numbers.
+const age = "99_999_999"
+
+console.log()
+
+//6. Boolean
+
+let isAdmin = true
+
+isAdmin = false
+
+//7. Arrays
+const list = [
+    "Brennan"
+]
+
+console.log( list )
+
+// 8. Objects
+
+const students = [
+    {
+        firstName:"Brennan",
+        lastName: "Whitworth",
+        age: 22,
+        isAdmin: true
+    },
+    {
+        firstName:"Bailey",
+        lastName: "Mooney",
+        age: 21,
+        isAdmin: false
+    },
+]
+
+console.log(students[1].lastName)
+
+// 9. Operators 
