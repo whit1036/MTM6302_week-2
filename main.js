90240da3fa0ee95b8b3bcdb50fb3cd17a2380ec6
Brennan Whitworth <whit1036@algonquinlive.com>
2023-01-19 09:09:24 -0500

    test

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('Hello')
+// Variables
+
+// avoid keywords
+// avoid variable names already assigned
+
+//var firstName = "Brennan";
+//firstName = "Jane";
+//console.log(firstName)
+
+//Let not going to change
+
+//let secondName = "Whitworth";
+{
+    //let secondName = "Brennan";
+}
+//console.log(secondName)
+
+//Const going to change
+
+const firstName = "Joe"
+
+const firstName = "Jane"
+
+console.log(firstName)
